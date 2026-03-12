a940db2c8acd22526a5432f27e1c58eca723b2a2
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2026-03-12 14:32:27 -0300

    Create ThemeContext.jsx

---
+++ frontend/src/contexts/ThemeContext.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }, [isDark]);
+
+  const toggleTheme = () => setIsDark(!isDark);
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
