c9831369fd118cc18748293ee9a4769a78fb2342
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2026-03-12 14:31:12 -0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Planner from './pages/Planner';
+import Notebooks from './pages/Notebooks';
+import NotebookDetail from './pages/NotebookDetail';
+import Podcasts from './pages/Podcasts';
+import PDFs from './pages/PDFs';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <div className="app">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/planner" element={<Planner />} />
+              <Route path="/notebooks" element={<Notebooks />} />
+              <Route path="/notebooks/:id" element={<NotebookDetail />} />
+              <Route path="/podcasts" element={<Podcasts />} />
+              <Route path="/pdfs" element={<PDFs />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
